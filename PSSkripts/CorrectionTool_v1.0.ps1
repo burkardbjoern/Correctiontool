5134ba890339609844ed1508e02a0ab212d5073b
Burkard Björn <bjoern.burkard@calida.com>
2017-12-21 13:57:54 +0100

    v4.1

--- PSSkripts/CorrectionTool_v1.0.ps1
+++ PSSkripts/CorrectionTool_v1.0.ps1
@@ -1,7 +1,9 @@
 ﻿$endung=get-item "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\" | get-ItemProperty | select HideFileExt | format-wide | out-string
 $ipv6=Get-NetAdapterBinding | Select DisplayName,Enabled | where DisplayName -like "Internetprotokoll, Version 6 (TCP/IPv6)" | out-string
 $IPGatSub=Ipconfig | sls Gate,Sub,IPv4 | Out-String
+$Tastatur=Get-Item "HKCU:\Control Panel\International\User Profile\de-CH" | Get-ItemProperty | select 0807:00000413 | out-string
 $DNS=Get-DnsClientServerAddress | select ServerAddresses -First 1 |format-wide | out-string
+$Solitaire=Get-AppxPackage | select Name | where Name -Like *Solitaire* | Format-List| out-string
 $DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
 $Textdatei = "$DesktopPath\CorrectionTool.txt"
 echo "------------------------------"|Out-File $Textdatei
@@ -25,10 +27,35 @@ else
 
 "Aufgabe 2.1 | IPv6 Protokoll deaktivieren"|Out-File $Textdatei -append
 "Ergebnis: $ipv6"|Out-File $Textdatei -append
+
 "Aufgabe 2.2 | IP-Adresse: 172.16.100.100/ Subnetmaske: 255.255.255.0/ Gateway: 172.16.100.1"|Out-File $Textdatei -append
 $IPGatSub|Out-File $Textdatei -append
 "DNS:"|Out-File $Textdatei -append
 $DNS|Out-File $Textdatei -append
+
+"Aufgabe 3.5 | Die Niederländische Tastatur für Hans einrichten"|Out-File $Textdatei -append
+"Ergebnis: "|Out-File $Textdatei -append
+if($Tastatur -match "2")
+{
+    "Die Niederländische Tastatur wurde hinzugefügt"|Out-File $Textdatei -append
+}
+else
+{
+    "Die Niederländische Tastatur wurde nicht hinzugefügt"|Out-File $Textdatei -append
+}
+
+
+
+"Aufgabe 5 | Bei Hans die App Microsoft Solitaire und Office holen entfernen"|Out-File $Textdatei -append
+"Ergebnis: "|Out-File $Textdatei -append
+if($Solitaire -eq "")
+{
+    "Die App Microsoft Solitaire ist deinstalliert"|Out-File $Textdatei -append
+}
+else
+{
+    "Die App Microsoft Solitaire ist vorhanden"|Out-File $Textdatei -append
+}
 
 
 cls
